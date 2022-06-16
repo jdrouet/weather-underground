2cd76cf9051d458b524768e0032851da3ffae17e
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-06-16 09:40:13 +0200

    chore: upgrade dependencies
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,19 +1,39 @@
-use clap::{crate_version, Clap};
+use clap::Parser;
+use std::convert::TryFrom;
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
 use std::time::Duration;
 use weather_underground as wu;
-use std::convert::TryFrom;
 
-#[derive(Clap, Debug)]
-#[clap(
-    version = crate_version!(),
-    author = "Jeremie Drouet <jeremie.drouet@gmail.com>"
-)]
+#[derive(Debug)]
+struct Unit(wu::Unit);
+
+impl FromStr for Unit {
+    type Err = Error;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "m" => Ok(Unit(wu::Unit::Metric)),
+            "e" => Ok(Unit(wu::Unit::English)),
+            _ => Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Invalid Unit value, options are 'm' for metric and 'e' for imperial.",
+            )),
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
 struct Options {
-    #[clap(short, long, about = "Timeout in ms", default_value = "10000")]
+    /// Timeout in ms
+    #[clap(short, long, default_value = "10000")]
     pub timeout: u64,
-    #[clap(short, long, about = "Unit (m for metric, e for imperial)", default_value = "m")]
-    pub unit: wu::Unit,
-    #[clap(about = "ID of the station you want the observations from")]
+    /// Unit (m for metric, e for imperial)
+    #[clap(short, long, default_value = "m")]
+    pub unit: Unit,
+    /// ID of the station you want the observations from
+    #[clap()]
     pub station_id: String,
 }
 
@@ -30,13 +50,24 @@ async fn main() {
     let opts = Options::parse();
 
     let client = wu::create_client(opts.get_timeout()).expect("couldn't create client");
-    let api_key = wu::fetch_api_key(&client).await.expect("couldn't fetch api key");
-    let result = wu::fetch_observation(&client, api_key.as_str(), opts.station_id.as_str(), &opts.unit).await.expect("couldn't fetch observation");
+    let api_key = wu::fetch_api_key(&client)
+        .await
+        .expect("couldn't fetch api key");
+    let result = wu::fetch_observation(
+        &client,
+        api_key.as_str(),
+        opts.station_id.as_str(),
+        &opts.unit.0,
+    )
+    .await
+    .expect("couldn't fetch observation");
     if let Some(value) = result {
         let result = wu::ObservationResponse::try_from(value).expect("couldn't parse response");
-        println!("{}", result.to_json().expect("couldn't format observations"));
+        println!(
+            "{}",
+            result.to_json().expect("couldn't format observations")
+        );
     } else {
         eprintln!("no result...");
     }
 }
-       
